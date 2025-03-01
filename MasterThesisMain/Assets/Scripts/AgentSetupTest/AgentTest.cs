7946115981c5dbcd48b9ff0b708482c3efff2bd1
mwpodgorni <mwpodgorni@gmail.com>
2025-03-01 17:59:25 +0100

    update

--- MasterThesisMain/Assets/Scripts/AgentSetupTest/AgentTest.cs
+++ MasterThesisMain/Assets/Scripts/AgentSetupTest/AgentTest.cs
@@ -1,40 +1,31 @@
 using UnityEngine;
 using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class AgentTest : Agent
+public class AgentLeftRight : Agent
 {
     public GameObject smallTarget;
     public GameObject largeTarget;
-    private Rigidbody m_AgentRb;
-    const int k_MinPosition = 0;
-    const int k_MaxPosition = 20;
-    const float maxDistanceFromLargeTarget = 20f;  // Max distance before reset
-    private float initialDistanceToLargeTarget;
+    private float moveForce = 0.5f;
+    Rigidbody m_AgentRb;
+    private float timeLimit = 8f;
+    private float timer = 0f;
 
     public override void Initialize()
     {
         m_AgentRb = GetComponent<Rigidbody>();
-        m_AgentRb.useGravity = true;
-        initialDistanceToLargeTarget = Vector3.Distance(transform.localPosition, largeTarget.transform.localPosition);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(smallTarget.transform.localPosition);
-        sensor.AddObservation(largeTarget.transform.localPosition);
-    }
-
-    public override void OnActionReceived(ActionBuffers actions)
-    {
-        MoveAgent(actions.DiscreteActions);
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(smallTarget.transform.position);
+        sensor.AddObservation(largeTarget.transform.position);
     }
 
     public void MoveAgent(ActionSegment<int> act)
     {
-        Debug.Log("MoveAgent" + act[0]);
         var dirToGo = Vector3.zero;
         var rotateDir = Vector3.zero;
 
@@ -48,58 +39,69 @@ public class AgentTest : Agent
                 dirToGo = transform.forward * -1f;
                 break;
             case 3:
-                rotateDir = transform.up * 1f;
+                dirToGo = transform.right * 1f;
                 break;
             case 4:
-                rotateDir = transform.up * -1f;
+                dirToGo = transform.right * -1f;
                 break;
         }
-
         transform.Rotate(rotateDir, Time.deltaTime * 150f);
-        Debug.Log("dirToGo" + dirToGo);
-        m_AgentRb.AddForce(dirToGo, ForceMode.VelocityChange);
+        m_AgentRb.AddForce(dirToGo * moveForce, ForceMode.VelocityChange);
+    }
 
-        // Calculate the distance to the large target
-        float currentDistanceToLargeTarget = Vector3.Distance(transform.localPosition, largeTarget.transform.localPosition);
-        float distanceReward = Mathf.Clamp01((initialDistanceToLargeTarget - currentDistanceToLargeTarget) / initialDistanceToLargeTarget);
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        AddReward(-0.01f);
+        MoveAgent(actionBuffers.DiscreteActions);
+        float distanceToLargeTarget = Vector3.Distance(transform.position, largeTarget.transform.position);
+        AddReward(1f / (distanceToLargeTarget + 1f));  // Higher reward the closer to the large target
+    }
 
-        // Reward the agent for moving closer to the large target
-        SetReward(distanceReward);
-
-        // Check if agent reaches the small target
-        if (gameObject.transform.localPosition == smallTarget.transform.localPosition)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == smallTarget)
         {
-            SetReward(0.1f);
+            AddReward(0.1f);
             EndEpisode();
-            ResetAgent();
         }
-
-        // Check if agent reaches the large target
-        if (gameObject.transform.localPosition == largeTarget.transform.localPosition)
+        else if (other.gameObject == largeTarget)
         {
-            SetReward(1f);  // Reward for reaching the large target
+            AddReward(1f);
             EndEpisode();
-            ResetAgent();
         }
+    }
 
-        // Check if the agent is too far from the large target
-        if (currentDistanceToLargeTarget > maxDistanceFromLargeTarget)
-        {
-            Debug.Log("Agent is too far from large target, resetting...");
-            EndEpisode();
-            ResetAgent();
-        }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = 0;
+        if (Input.GetKey(KeyCode.W)) discreteActions[0] = 1;
+        else if (Input.GetKey(KeyCode.S)) discreteActions[0] = 2;
+        else if (Input.GetKey(KeyCode.D)) discreteActions[0] = 3;
+        else if (Input.GetKey(KeyCode.A)) discreteActions[0] = 4;
     }
 
     public override void OnEpisodeBegin()
     {
-        ResetAgent();
+        transform.position = new Vector3(0f, 1f, 0f);
+        m_AgentRb.velocity = Vector3.zero;
+        timer = 0f;
     }
 
-    void ResetAgent()
+    private void Update()
     {
-        transform.localPosition = new Vector3(0, 0.5f, 0);
+        timer += Time.deltaTime;
+
+        if (timer >= timeLimit)
+        {
+            ResetAgent();
+            EndEpisode();
+        }
+    }
+
+    private void ResetAgent()
+    {
+        transform.position = new Vector3(0f, 1f, 0f);
         m_AgentRb.velocity = Vector3.zero;
-        initialDistanceToLargeTarget = Vector3.Distance(transform.localPosition, largeTarget.transform.localPosition);
     }
 }
