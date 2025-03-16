4cb87b57c7335ef3bf5c2ade2d39a419f04ceace
mwpodgorni <mwpodgorni@gmail.com>
2025-03-16 15:35:41 +0100

    main menu basic setup

---
+++ MasterThesisMain/Assets/UI Toolkit/MainMenuController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    public VisualElement ui;
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button quitButton;
+    public VisualElement optionsPanel;
+    private void Awake()
+    {
+        ui = GetComponent<UIDocument>().rootVisualElement;
+    }
+    private void OnEnable()
+    {
+        playButton = ui.Q<Button>("PlayButton");
+        playButton.clicked += OnPlayButtonClicked;
+        optionsButton = ui.Q<Button>("OptionsButton");
+        optionsButton.clicked += OnOptionsClicked;
+        quitButton = ui.Q<Button>("QuitButton");
+        quitButton.clicked += OnQuitButtonClicked;
+
+        optionsPanel = ui.Q<VisualElement>("OptionsPanel");
+        optionsPanel.style.display = DisplayStyle.None;
+    }
+    private void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+    private void OnOptionsClicked()
+    {
+        Debug.Log("OnOptionsClicked");
+        optionsPanel.style.display = optionsPanel.style.display == DisplayStyle.None
+             ? DisplayStyle.Flex
+             : DisplayStyle.None;
+    }
+    private void OnPlayButtonClicked()
+    {
+        Debug.Log("Play Button Clicked");
+    }
+
+}
