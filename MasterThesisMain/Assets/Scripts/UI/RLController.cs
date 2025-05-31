fc69e168c2440b409a255a6f76d7a742718bd31d
Alex Su <74763673+alexw-su@users.noreply.github.com>
2025-05-31 12:22:29 +0200

    Paper version (#8)
    Co-authored-by: mwpodgorni <mwpodgorni@gmail.com>

--- MasterThesisMain/Assets/Scripts/UI/RLController.cs
+++ MasterThesisMain/Assets/Scripts/UI/RLController.cs
@@ -792,9 +792,7 @@ public class RLController : MonoBehaviour
         if (ActivityTracker.Instance != null)
         {
             Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSffHAoJXE4RetOsmDZG0FycdND9QlhSbru142JqOCFz9zDUAQ/viewform?usp=pp_url&entry.978412280=" + ActivityTracker.Instance.GetSessionId());
-
         }
-
         Application.Quit();
     }
     public void HideEvaluationStatus()
